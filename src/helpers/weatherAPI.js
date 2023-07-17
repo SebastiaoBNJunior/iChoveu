21ee40bbe1cd632553153a48b5f770e8c8f2f757
Sebastiaobnj <junin_sbn@hotmail.com>
2023-07-17 13:30:29 -0300

    testando requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,15 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN; // Importando o token do arquivo .env
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  try {
+    const resolve = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+    const cities = await resolve.json();
+    if (cities.length === 0) {
+      throw new Error();
+    }
+  } catch (error) {
+    alert('Nenhuma cidade encontrada');
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
