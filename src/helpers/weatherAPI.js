8ca4e592ca6131750e2e24188ad7dd9a6c1b9549
Sebastiaobnj <junin_sbn@hotmail.com>
2023-07-17 15:56:13 -0300

    testando commit depois do login git

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -2,7 +2,7 @@ const TOKEN = import.meta.env.VITE_TOKEN; // Importando o token do arquivo .env
 
 export const searchCities = async (term) => {
   try {
-    const resolve = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+    const resolve = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`); // habilitando a api de pesquisa para o nome da cidade
     const cities = await resolve.json();
     if (cities.length === 0) {
       throw new Error();
